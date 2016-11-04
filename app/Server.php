15c309ba40d57ce966e8726d653bc905340fafbe
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-11-03 22:55:52 -0600

    Added server configuration

---
+++ app/Server.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Server configuration, do not commit sensitive data!
+ */
+
+define('DOMAIN', 'domain'); 
+define('ROOT_PATH', $_SERVER['ROOT_DIR'].'whatadesktop/');
+define('DB_HOST', 'host');
+define('DB_USER', 'username');
+define('DB_PASSWD', 'password'); 
+define('DB_NAME', 'name');
+define('CACHE_DIR', ROOT_PATH . "dir");
+define('AUTOLOAD_CACHE_PATH', CACHE_DIR . "/file");
