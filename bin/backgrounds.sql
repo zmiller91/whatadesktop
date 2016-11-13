123d6ec79724f6ef1649e0a07b2f5ee0135fda75
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-11-12 21:54:06 -0700

    Updated DB Creation Script
        Added indexes to a few tables

--- bin/backgrounds.sql
+++ bin/backgrounds.sql
@@ -4,7 +4,7 @@ create table images(
     id int not null auto_increment,
     filehash varchar(64),
     filename varchar(256),
-	root varchar(64),
+    root varchar(64),
     path varchar(256),
     width int,
     height int,
@@ -13,6 +13,7 @@ create table images(
     saved tinyint default 0,
     
     primary key (id),
+    index (root),
     unique(filehash)
     
 );
@@ -42,11 +43,12 @@ create table user_sessions(
 create table img_status(
     user int not null,
     img_root varchar(256) not null,
-	img_id int not null,
+    img_id int not null,
     status int not null,
     updated_date datetime,
     primary key(user, img_id),
     index (user, img_root),
+    index (img_root),
     foreign key(user) references users(id),
     foreign key(img_id) references images(id)
 );
